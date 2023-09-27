f152c85175c56ec642120b9cb77564cf8d5fc605
jgr599 <114884420+jgr599@users.noreply.github.com>
2023-09-27 13:56:31 +0800

    Add files via upload

--- 04_variable.php
+++ 04_variable.php
@@ -1,26 +1,30 @@
-<!-- ARRAY -->
 <?php
-$cars = array("Volvo","BMW","Toyota");
-var_dump($cars);
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+?>
+
+<!-- DOUBLE -->
+<?php
+$many = 2.2888800;
+$many_2 = 2.2111200;
+$few = $many + $many_2;
+echo $few . " = " . "is a " . gettype($few) . "<br>";
+
 ?>
 
 <!-- INTEGER -->
 
 <?php
 $int = 5985;
-var_dump($x);
+echo $int . ": " . "is an ". gettype($int) . "<br>";
+
 ?>
 
-<!-- FLOAT -->
-<?php
-$float = 10.365;
-var_dump($x);
-?>
 
 <!-- STRING -->
 <?php
 $x = "Hello world!";
+echo $x . "<br>";
 
-echo $x;
-echo "<br>";
 ?>
