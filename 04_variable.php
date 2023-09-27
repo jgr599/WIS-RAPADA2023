ae5d90b2aa9044dabe16642a9f62a4636df03d18
jgr599 <114884420+jgr599@users.noreply.github.com>
2023-09-27 12:48:53 +0800

    Add files via upload

---
+++ 04_variable.php
@@ -0,0 +1,26 @@
+<!-- ARRAY -->
+<?php
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+?>
+
+<!-- INTEGER -->
+
+<?php
+$int = 5985;
+var_dump($x);
+?>
+
+<!-- FLOAT -->
+<?php
+$float = 10.365;
+var_dump($x);
+?>
+
+<!-- STRING -->
+<?php
+$x = "Hello world!";
+
+echo $x;
+echo "<br>";
+?>
