c963e31e6d74c82ac877cdc2f3d7607393d4c7e9
JhonR <rapada.jhoniel@gmail.com>
2023-10-05 09:36:58 +0800

    comment

--- 04_variable.php
+++ 04_variable.php
@@ -1,4 +1,30 @@
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 
-echo $x;
-echo "<br>";
+?>
+
+<!-- DOUBLE -->
+<?php
+$many = 2.2888800;
+$many_2 = 2.2111200;
+$few = $many + $many_2;
+echo $few . " = " . "is a " . gettype($few) . "<br>";
+
+?>
+
+<!-- INTEGER -->
+
+<?php
+$int = 5985;
+echo $int . ": " . "is an ". gettype($int) . "<br>";
+
+?>
+
+
+<!-- STRING -->
+<?php
+$x = "Hello world!";
+echo $x . "<br>";
+
 ?>
